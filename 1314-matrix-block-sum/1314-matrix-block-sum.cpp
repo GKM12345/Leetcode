6889b140e8c669ad9dcdaa9f7ce2918784f3ce18
Gaurav Kumar Mishra <72800989+GKM12345@users.noreply.github.com>
2022-08-21 02:59:11 +0530

    Time: 14 ms (70.01%), Space: 9.2 MB (96.72%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -1,33 +1,45 @@
 class Solution {
-public: 
-    int sizeX, sizeY;
-    int extractSum(int i, int j, const vector<vector<int> >& sum) {
-        if (i < 0 || j < 0) return 0;
-        if (i >= sizeX) i = sizeX - 1;
-        if (j >= sizeY) j = sizeY - 1;
-        return sum[i][j];
-    }
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         
-    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int K) {
-        sizeX = mat.size();
-        sizeY = mat[0].size();
+        int m=mat.size();
+        int n=mat[0].size();
         
-        vector<vector<int>> sum(sizeX, vector<int>(sizeY, 0));
-        // Calculate prefix matrix 
-        for (int i = 0; i < sizeX; i++) {
-            for (int j = 0; j < sizeY; j++) {
-                sum[i][j] = mat[i][j] + extractSum(i-1, j, sum) + extractSum(i, j-1, sum) - extractSum(i-1, j-1, sum);
+        vector<vector<int>> ans(m,vector<int>(n));
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                mat[i][j]=mat[i][j]+mat[i][j-1];
             }
         }
         
-        // Use prefix matrix to calculate our sum
-        vector<vector<int>> ans(sizeX, vector<int>(sizeY, 0));
-        for (int i = 0; i < sizeX; i++) {
-            for (int j = 0; j < sizeY; j++) {
-                ans[i][j] = extractSum(i+K, j+K, sum) - extractSum(i+K, j-K-1,sum) - extractSum (i-K-1, j+K, sum) + extractSum(i-K-1, j-K-1, sum);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                mat[j][i]=mat[j][i]+mat[j-1][i];
+            }
+        }
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int i1=max(0,i-k);
+                int i2=min(m-1,i+k);
+                int j1=max(0,j-k);
+                int j2=min(n-1,j+k);
+                
+                ans[i][j]=mat[i2][j2];
+                if(j1>0) ans[i][j]-=mat[i2][j1-1];
+                if(i1>0) ans[i][j]-=mat[i1-1][j2];
+                if(i1>0 && j1>0) ans[i][j]+=mat[i1-1][j1-1];
             }
         }
         
         return ans;
+        
     }
 };
