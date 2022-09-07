61773db23e53475b543d27f3fc87a0447dad40d6
Gaurav Kumar Mishra <72800989+GKM12345@users.noreply.github.com>
2022-09-07 13:37:37 +0530

    Time: 490 ms (18.37%), Space: 81.9 MB (84.29%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.cpp
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -3,48 +3,18 @@ public:
     int maximumGap(vector<int>& nums) {
         
         int n=nums.size();
+        sort(nums.begin(),nums.end());
+        
         if(n==1) return 0;
-        
-        vector<pair<int,int>> bucket(n,{INT_MAX,INT_MIN});
-        
-        int mini=INT_MAX,maxi=INT_MIN;
-        for(int i=0;i<n;i++)
+        int ans=INT_MIN;
+        for(int i=0;i<n-1;i++)
         {
-            mini=min(mini,nums[i]);
-            maxi=max(maxi,nums[i]);
-        }
-        int gap;
-        if((maxi-mini)%(n-1)==0)
-        {
-            gap=(maxi-mini)/(n-1);
-        }
-        else
-        {
-            gap=(maxi-mini)/(n-1)+1;
-        }
-        if(gap==0) return 0;
-        
-        for(int i=0;i<n;i++)
-        {
-            int index=(nums[i]-mini)/gap;
-            bucket[index].first=min(bucket[index].first,nums[i]);
-            bucket[index].second=max(bucket[index].second,nums[i]);
-        }
-        
-        int ans=gap;
-        int val1=bucket[0].second;
-        for(int i=1;i<n;i++)
-        {
-            if(bucket[i].first!=INT_MAX)
+            if((nums[i+1]-nums[i])>ans)
             {
-                ans=max(ans,bucket[i].first-val1);
-                val1=bucket[i].second;
+                ans=nums[i+1]-nums[i];
             }
         }
         
         return ans;
-        
-        
-        
     }
 };
