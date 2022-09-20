b81e79f3c0802a7031213f0d18abff180c0a5b0e
Gaurav Kumar Mishra <72800989+GKM12345@users.noreply.github.com>
2022-09-20 12:12:46 +0530

    Time: 3 ms (85.40%), Space: 7.6 MB (82.79%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,40 @@
+bool cmp(pair<int,int> &p1, pair<int,int> &p2)
+{
+    if(p1.second!=p2.second)
+    {
+        return p1.second<p2.second;
+    }
+    return p1.first<p2.first;
+}
+
+
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        
+        int n=arr1.size(),m=arr2.size();
+        vector<int> stm(1001,INT_MAX);
+        
+        for(int i=0;i<m;i++)
+        {
+            stm[arr2[i]]=i;
+        }
+        
+        vector<pair<int,int>> v;
+        
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({arr1[i],stm[arr1[i]]});
+        }
+        
+        sort(v.begin(),v.end(),cmp);
+        
+        for(int i=0;i<n;i++)
+        {
+            arr1[i]=v[i].first;
+        }
+        
+        return arr1;
+        
+    }
+};
