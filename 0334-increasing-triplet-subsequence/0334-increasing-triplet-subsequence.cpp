dcd30e7cbe845c1a0fa4a125e0c32fab79b39dd7
Gaurav Kumar Mishra <72800989+GKM12345@users.noreply.github.com>
2022-10-12 01:21:53 +0530

    Time: 121 ms (55.50%), Space: 63.6 MB (13.59%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -8,26 +8,22 @@ public:
             return false;
         }
         
-        int premin[n],sufmax[n];
+        int premin[n];
         premin[0]=nums[0];
         for(int i=1;i<n;i++)
         {
             premin[i]=min(nums[i],premin[i-1]);
         }
-        sufmax[n-1]=nums[n-1];
+        int sufmax=nums[n-1];
         for(int i=n-2;i>=0;i--)
         {
-            sufmax[i]=max(nums[i],sufmax[i+1]);
-        }
-        
-        
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>premin[i] && nums[i]<sufmax[i])
+            if(nums[i]>premin[i] && nums[i]<sufmax)
             {
                 return true;
             }
+            sufmax=max(nums[i],sufmax);
         }
+        
         
         return false;
         
