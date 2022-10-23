ad22e8d84ec1d9a09697beba91e211a7a2fd9487
Gaurav Kumar Mishra <72800989+GKM12345@users.noreply.github.com>
2022-10-23 15:46:37 +0530

    Time: 326 ms (20.23%), Space: 12.5 MB (51.63%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -6,24 +6,21 @@ public:
         int n=arr.size();
         for(int i=0;i<n;i++)
         {
+            int sq=sqrt(arr[i]);
+            if(sq*sq==arr[i])
+            {
+                continue;
+            }
             long long sum=0;
             int count=0;
             for(int j=1;j*j<=arr[i];j++)
             {
                 if(arr[i]%j==0)
                 {
-                    if(j*j==arr[i])
-                    {
-                        count+=1;
-                        sum+=j;
-                    }
-                    else
-                    {
-                        count+=2;
-                        sum+=j;
-                        sum+=arr[i]/j;
-                    }
-                    
+                    // cout << j << " " << arr[i]/j << endl;
+                    count+=2;
+                    sum+=j;
+                    sum+=arr[i]/j;
                 }
             }
             if(count==4)
