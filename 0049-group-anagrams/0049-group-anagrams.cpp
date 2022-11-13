5e468baf9647e06735837e8f2c663bcd5b0dac9d
Gaurav Kumar Mishra <72800989+GKM12345@users.noreply.github.com>
2022-11-13 19:07:28 +0530

    Time: 85 ms (35.52%), Space: 21.8 MB (20.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -6,7 +6,7 @@ public:
         
         int n=strs.size();
         
-        map<string,vector<string>> mp;
+        unordered_map<string,vector<string>> mp;
             
         for(int i=0;i<n;i++)
         {
